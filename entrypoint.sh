9084b6c85912e59b171bf82b6be560340b8b1257
Benjamin Borbe <bborbe@rocketnews.de>
2016-11-29 00:14:29 +0100

    remove slapd.ldif + encrypt pw

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,20 +8,20 @@ set -o errtrace
 ulimit -n 8192
 
 if [ "$1" = 'slapd' ]; then
-	ldap_secret=${LDAP_SECRET:-'bIgGMKt5nWX3fL9EaPZ86Aklrb587fy1'}
+	ldap_secret=${LDAP_SECRET:-'S3CR3T'}
 	ldap_suffix=${LDAP_SUFFIX:-'dc=my-domain,dc=com'}
 	ldap_rootdn=${LDAP_ROOTDN:-'cn=Manager,dc=my-domain,dc=com'}
 
+	ldap_secret=`slappasswd -s ${ldap_secret}`
+	unset LDAP_SECRET
+
 	sed_script=""
 	for var in ldap_secret ldap_suffix ldap_rootdn; do
-		sed_script+="s/{{$var}}/${!var}/g;"
+		sed_script+="s~{{$var}}~${!var}~g;"
 	done
 
 	echo "create slapd.conf"
 	cat /etc/openldap/slapd.conf.template | sed -e "$sed_script" > /etc/openldap/slapd.conf
-
-	echo "create slapd.ldif"
-	cat /etc/openldap/slapd.ldif.template | sed -e "$sed_script" > /etc/openldap/slapd.ldif
 
 	echo "starting openldap $@"
 fi
